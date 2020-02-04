ab20232d0ca6aa1bf65912e6cdb469ca6f3185ea
coolsgupta <cool.sgupta@gmail.com>
2020-02-03 18:11:33 -0800

    string implementations

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -15,6 +15,13 @@ class Solution(object):
             p = p*10
         return nnum + 3*fp
 
+    def usingString(self, num):
+        sn = str(num)
+        if sn == '9'*len(sn):
+            return num
+        i = sn.index('6')
+        return int(sn[:i] + '9' + sn[i+1:])
+
 
 obj = Solution()
 n = obj.maximum69Number(9999)
