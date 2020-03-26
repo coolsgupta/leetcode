903a0396e84d2a18b41c8107fb6506118f883ec7
Sachin Gupta <cool.sgupta@gmail.com>
2020-03-25 18:01:19 -0700

    squares-of-a-sorted-array_alternate

--- flipping-an-image.py
+++ flipping-an-image.py
@@ -5,3 +5,10 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         return [[0 if i else 1 for i in row[::-1]] for row in A]
+
+    def flipAndInvertImage_alternate(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[1 - x for x in row[::-1]] for row in A]
