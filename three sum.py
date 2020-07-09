f8b7011ba1dd77fe6262ff76664fe8c9166fa86f
Sachin Gupta <cool.sgupta@gmail.com>
2020-07-08 17:45:52 -0700

    attempting hashed version of the soln

---
+++ three sum.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+        triplets = {}
+        for x in range(len(nums) - 1):
+            for y in range(x + 1, len(nums)):
+                nums_dict = nums
+                pair = -(nums[x] + nums[y])
+                if pair in nums_dict:
+                    trip = tuple(sorted([nums[x], nums[y], pair]))
+                    if trip not in triplets:
+                        triplets[trip] = 0
+        return list(triplets.keys())
+
+
+class SolutionHashed(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 3:
+            return []
+
+        triplets = set()
+        for k, target in enumerate(nums[:-2]):
+            hashed_complements = {}
+            for i, x in enumerate(nums[k + 1:]):
+                comp = -(target + x)
+
+                if comp in hashed_complements:
+                    triplets.add(tuple(sorted([target, comp, x])))
+
+                hashed_complements[x] = i
+
+        return list(triplets)
