62fb588db4cfaff1e136bc02867aa02a15740fbd
Sachin Gupta <cool.sgupta@gmail.com>
2020-10-06 20:58:29 -0700

    optimized code

--- diagonal_arranging.py
+++ diagonal_arranging.py
@@ -1,8 +1,10 @@
 def get_rows(grid):
     return [[c for c in r] for r in grid]
 
+
 def get_cols(grid):
     return zip(*grid)
+
 
 def get_diagonals(grid):
     b = [None] * (len(grid) - 1)
@@ -12,26 +14,9 @@ def get_diagonals(grid):
 
 def diagonalsArranging(a):
     diagonals = get_diagonals(a)
-    cyclic_diagonals = []
-    d_map = {}
-    for i, diagonal in enumerate(diagonals):
-        inc = len(a)//len(diagonal) + 1
-        cycle = diagonal*inc
-        cycle = ''.join(cycle[:len(a)])
-        cyclic_diagonals.append(''.join(cycle))
-        if cycle not in d_map:
-            d_map[cycle] = []
-        d_map[cycle].append(i+1)
-
-    cyclic_diagonals = sorted(cyclic_diagonals)
-
-    indices = []
-    for i in range(len(cyclic_diagonals)):
-        if cyclic_diagonals[i] == cyclic_diagonals[i-1]:
-            continue
-        indices.extend(sorted(d_map.get(cyclic_diagonals[i])))
-
-    return indices
+    n = len(a)
+    cyclic_diagonals = sorted([("".join((diag*(n//len(diag) + 1))[:n]), i) for i, diag in enumerate(diagonals)])
+    return [x[1] + 1 for x in cyclic_diagonals]
 
 a = [[ "a", "c", "a", "b", "b"],
 [ "c", "b", "a", "c", "b"],
@@ -40,4 +25,5 @@ a = [[ "a", "c", "a", "b", "b"],
 [ "a", "b", "e", "b", "a"]]
 
 ans = diagonalsArranging(a)
+print(ans)
 
