6d26240f826078796fc8fe0d120943834d32007a
Sachin Gupta <cool.sgupta@gmail.com>
2020-10-07 00:25:17 -0700

    modified code for a one line response

--- matrix_summation.py
+++ matrix_summation.py
@@ -5,14 +5,27 @@ def get_elem(x,y, matrix):
 
 
 def findBeforeMatrix(after):
-    before = []
-    for x in range(len(after)):
-        before.append([])
-        for y in range(len(after[0])):
-            element = get_elem(x, y, after) - get_elem(x-1, y, after) - get_elem(x, y-1, after)+ get_elem(x-1, y-1, after)
-            before[x].append(element)
+    # detailed response
+    # before = []
+    # for x in range(len(after)):
+    #     before.append([])
+    #     for y in range(len(after[0])):
+    #         element = get_elem(x, y, after) - get_elem(x-1, y, after) - get_elem(x, y-1, after) + get_elem(x-1, y-1, after)
+    #         before[x].append(element)
+
+    # list comprehension version of response
+    before = [
+        [
+            get_elem(i, j, after) - get_elem(i - 1, j, after) - get_elem(i, j - 1, after) + get_elem(i - 1, j - 1, after)
+            for j in range(len(after[0]))
+        ]
+        for i in range(len(after))
+    ]
 
     return before
+
+
+if __name__ == '__main__':
 
 after = [[2, 5], [7, 17]]
 ans = findBeforeMatrix(after)
