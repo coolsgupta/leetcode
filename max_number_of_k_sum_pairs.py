4f7a0d711b10d8309a38cf03622fb4693924f86d
Sachin Gupta <cool.sgupta@gmail.com>
2021-01-20 18:40:28 -0800

    max_number_of_k_sum_pairs.py

---
+++ max_number_of_k_sum_pairs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count_map = {}
+        pair_count = 0
+        for x in nums:
+            if x not in count_map:
+                count_map[x] = 0
+
+            count_map[x] += 1
+
+            rem = k - x
+            if rem not in count_map:
+                continue
+
+            else:
+                if count_map[rem] >= 1:
+                    count_map[x] -= 1
+                    count_map[rem] -= 1
+
+                    if count_map[x] == -1:
+                        count_map[x] += 2
+                        continue
+
+                    pair_count += 1
+
+        return pair_count
+:wq
+wq
+
