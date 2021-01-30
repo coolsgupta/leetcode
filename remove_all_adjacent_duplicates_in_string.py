6946cdfadd03b0fbdc84211a3262b321fa20665b
Sachin Gupta <cool.sgupta@gmail.com>
2021-01-30 00:21:26 -0800

    remove_all_adjacent_duplicates_in_string

---
+++ remove_all_adjacent_duplicates_in_string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stack = []
+        for x in S:
+            if stack and stack[-1] == x:
+                stack.pop()
+            else:
+                stack.append(x)
+
+        return ''.join(stack)
