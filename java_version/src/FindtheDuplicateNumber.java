5fec9cc788db11f2d8534cbf5d56c7620de06124
Sachin Gupta <cool.sgupta@gmail.com>
2021-02-02 10:29:53 -0800

    Find the Duplicate Number

--- java_version/src/FindtheDuplicateNumber.java
+++ java_version/src/FindtheDuplicateNumber.java
@@ -8,4 +8,21 @@ public class FindtheDuplicateNumber {
 
         return nums[i];
     }
+
+    // using Floyd's Hare and Tortoise algorithms (for cycle detection in linked list)
+    public int findDuplicateOptimized(int[] nums) {
+        int hare = nums[0];
+        int tortoise = nums[0];
+
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        }while(hare != tortoise);
+        tortoise = nums[0];
+        while(hare != tortoise){
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+        return hare;
+    }
 }
