0dccdbe4b4f36a34a4874a533932f79730d366c6
Sachin Gupta <cool.sgupta@gmail.com>
2021-02-01 22:12:27 -0800

    NumberOfProvinces

--- java_version/src/NumberOfProvince.java
+++ java_version/src/NumberOfProvince.java
@@ -3,13 +3,12 @@ public class NumberOfProvince {
     int n;
 
     public void dfs(int i, int j){
-        if (0<=i && i<this.n && 0<=j && j<this.n && this.grid[i][j] == 1){
-            this.grid[i][j] = 0;
-            this.grid[j][i] = 0;
-            this.dfs(i-1, j);
-            this.dfs(i, j-1);
-            this.dfs(i+1, j);
-            this.dfs(i, j+1);
+        for(int x=j; x<this.n; x++){
+            if (this.grid[i][x] == 1){
+                this.grid[i][x] = 0;
+                this.grid[x][i] = 0;
+                this.dfs(x, 0);
+            }
         }
     }
 
@@ -21,8 +20,9 @@ public class NumberOfProvince {
         for(int i=0; i<this.n; i++){
             for(int j=0; j<this.n; j++){
                 if (this.grid[i][j] == 1){
-                    provinces++;
                     this.dfs(i, j);
+                    provinces++;
+                    break;
                 }
             }
         }
